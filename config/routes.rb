d31ff034ecabc2216f2587dbf5ab8a14cdcce773
Misheck Livingi <livingim2018@gmail.com>
2023-10-09 17:37:38 +0000

    Oct 9, 2023, 8:37 PM

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   get 'dashboard/show'
-  devise_for :users, controllers: { registrations: 'users/registrations' } do
-    get 'new_freelancer', to: 'users/registrations#new_freelancer', as: :new_freelancer_registration
-    post 'create_freelancer', to: 'users/registrations#create_freelancer', as: :create_freelancer_registration
-  end
+
+  # Custom routes for freelancer registration by admin
+  get 'users/new_freelancer', to: 'users/registrations#new_freelancer', as: :new_freelancer_registration
+  post 'users/create_freelancer', to: 'users/registrations#create_freelancer', as: :create_freelancer_registration
+
+  # Devise routes
+  devise_for :users, controllers: { registrations: 'users/registrations' }
     
   resources :tasks
   resources :reviews, only: [:show, :new, :create, :edit, :update, :destroy]
 
   get 'dashboard', to: 'dashboard#show', as: 'dashboard'
-
 end
