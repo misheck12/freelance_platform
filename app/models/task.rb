117f8fb1be09b0fd1eb9c5a825ede03ae7b88f3f
Misheck Livingi <livingim2018@gmail.com>
2023-10-09 08:31:46 +0000

    Oct 9, 2023, 11:31 AM

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,17 @@
 class Task < ApplicationRecord
-  belongs_to :client, class_name: 'User'
-  belongs_to :freelancer, class_name: 'User', optional: true  
+  # Validations
+  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :budget, presence: true, numericality: { greater_than: 0 }
+  validates :deadline, presence: true
 
+  # Enum for status
   enum status: { open: 0, in_progress: 1, under_review: 2, completed: 3 }
 
+  # Scopes
+  scope :open_tasks, -> { where(status: 'open') }
+
+  # Associations
+  belongs_to :client, class_name: 'User'
+  belongs_to :freelancer, class_name: 'User', optional: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,6 @@
-class Task < ApplicationRecord
-  # Validations
-  validates :title, presence: true, length: { minimum: 5, maximum: 100 }
-  validates :description, presence: true, length: { minimum: 10 }
-  validates :budget, presence: true, numericality: { greater_than: 0 }
-  validates :deadline, presence: true
-
-  # Enum for status
-  enum status: { open: 0, in_progress: 1, under_review: 2, completed: 3 }
-
-  # Scopes
-  scope :open_tasks, -> { where(status: 'open') }
-
-  # Associations
-  belongs_to :client, class_name: 'User'
-  belongs_to :freelancer, class_name: 'User', optional: true
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
