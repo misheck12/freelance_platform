bc3a80fb0aa1bb9979498e6d109cc3abf8f449e7
Misheck Livingi <livingim2018@gmail.com>
2023-10-09 08:47:52 +0000

    Oct 9, 2023, 11:47 AM

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,10 @@ class Review < ApplicationRecord
 belongs_to :task
 belongs_to :reviewer, class_name: 'User'
 
+validates :rating, presence: true, inclusion: { in: 1..5 }
+validates :comment, length: { maximum: 500 }
+
+scope :five_star_reviews, -> { where(rating: 5) }
+
+
 end
