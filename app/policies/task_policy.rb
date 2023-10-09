86ddca213d33bc09bfee222772f1268f9cb16691
Misheck Livingi <livingim2018@gmail.com>
2023-10-09 13:28:07 +0000

    Oct 9, 2023, 4:28 PM

--- app/policies/task_policy.rb
+++ app/policies/task_policy.rb
@@ -1,21 +1,43 @@
 class TaskPolicy < ApplicationPolicy
+  # Define user's ability to view the task
+  def show?
+    user.admin? || record.client == user || (user.freelancer? && record.freelancer == user)
+  end
+
+  # Define user's ability to create a task
+  def create?
+    user.client?
+  end
+
+  # Define user's ability to update the task
+  def update?
+    user.admin? || record.client == user
+  end
+
+  # Define user's ability to delete the task
+  def destroy?
+    user.admin? || record.client == user
+  end
+
+  # Scope to determine which tasks a user can view in a list
   class Scope
     attr_reader :user, :scope
-  
+
     def initialize(user, scope)
       @user = user
       @scope = scope
     end
-  
+
     def resolve
       if user.admin?
         scope.all
+      elsif user.client?
+        scope.where(client: user)
       elsif user.freelancer?
         scope.where(freelancer: user)
       else
-        scope.where(client: user)
+        scope.none
       end
     end
   end
-  
 end
