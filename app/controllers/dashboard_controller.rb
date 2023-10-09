da35689835f5880be938bc4ed131de06d9674e83
Misheck Livingi <livingim2018@gmail.com>
2023-10-09 09:54:14 +0000

    Oct 9, 2023, 12:54 PM

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class DashboardController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    if current_user.client?
+      @tasks = current_user.tasks_as_client
+    elsif current_user.freelancer?
+      @tasks = current_user.tasks_as_freelancer
+    end
   end
 end
