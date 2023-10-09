8a1a9962b1c4c1d521c4de07a5158f620eee819d
Misheck Livingi <livingim2018@gmail.com>
2023-10-09 09:56:38 +0000

    Oct 9, 2023, 12:56 PM

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :tasks
   resources :reviews, only: [:show, :new, :create, :edit, :update, :destroy]
+
+  get 'dashboard', to: 'dashboard#show', as: 'dashboard'
+
 end
