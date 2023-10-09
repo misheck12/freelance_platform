b082a16e2e7dc2025d2d26d6916331f745c5f6dd
Misheck Livingi <livingim2018@gmail.com>
2023-10-09 08:26:22 +0000

    Oct 9, 2023, 11:26 AM

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
-  belongs_to :client
-  belongs_to :freelancer
+  belongs_to :client, class_name: 'User'
+  belongs_to :freelancer, class_name: 'User', optional: true  
+
+  enum status: { open: 0, in_progress: 1, under_review: 2, completed: 3 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+validates :title, presence: true, length: { minimum: 5, maximum: 100 }
+validates :description, presence: true, length: { minimum: 10 }
+validates :budget, presence: true, numericality: { greater_than: 0 }
+validates :deadline, presence: true
+
+
+         has_many :tasks_as_client, class_name: 'Task', foreign_key: 'client_id'
+         has_many :tasks_as_freelancer, class_name: 'Task', foreign_key: 'freelancer_id'
+         
+
          enum role: { admin: 0, client: 1, freelancer: 2 }
 
 end
