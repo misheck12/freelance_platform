c7f57768093ec125bd2e2b7fb237d44d03ede0e8
Misheck Livingi <livingim2018@gmail.com>
2023-10-09 10:24:34 +0000

    Oct 9, 2023, 1:24 PM

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@ class User < ApplicationRecord
   has_many :tasks_as_freelancer, class_name: 'Task', foreign_key: 'freelancer_id'
   has_many :given_reviews, class_name: 'Review', foreign_key: 'reviewer_id'
 
+  after_initialize :set_default_role, if: :new_record?
+
+  private
+
+  def set_default_role
+    self.role ||= :client
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'dashboard/show'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' } do
+    get 'new_freelancer', to: 'users/registrations#new_freelancer', as: :new_freelancer_registration
+    post 'create_freelancer', to: 'users/registrations#create_freelancer', as: :create_freelancer_registration
+  end
+    
   resources :tasks
   resources :reviews, only: [:show, :new, :create, :edit, :update, :destroy]
 
