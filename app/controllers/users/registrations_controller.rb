f42bbef0f2223d1bed07f8d3a02ff59ba8760a3c
Misheck Livingi <livingim2018@gmail.com>
2023-10-15 07:14:04 +0000

    Oct 15, 2023, 10:14 AM

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,47 +1,44 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-    before_action :configure_sign_up_params, only: [:create]
-    before_action :ensure_admin, only: [:new_freelancer, :create_freelancer]
-  
-    def new_freelancer
-      @user = User.new
-    end
-  
-    def create_freelancer
-      @user = User.new(sign_up_params.merge(role: :freelancer))
-      if @user.save
-        redirect_to root_path, notice: 'Freelancer was successfully created.'
-      else
-        render :new_freelancer
-      end
-    end
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :ensure_admin, only: [:new_freelancer, :create_freelancer, :destroy]
 
-    def destroy
-      resource.destroy
-      set_flash_message! :notice, :destroyed
-      respond_with_navigational(resource){ redirect_to dashboard_path } # Redirect to admin dashboard after deletion
-    end
-    
+  # GET /users/new_freelancer
+  def new_freelancer
+    @user = User.new
+  end
 
-    protected
-
-    def require_no_authentication
-      if current_user && !current_user.admin?
-        flash[:alert] = "You are already signed in."
-        redirect_to root_path
-      end
-      # Do not call the parent implementation, to avoid the redirection
-    end
-  
-    def configure_sign_up_params
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :role])
-    end    
-  
-    private
-  
-    def ensure_admin
-      unless current_user&.admin?
-        redirect_to root_path, alert: 'You are not authorized to perform this action.'
-      end
+  # POST /users/create_freelancer
+  def create_freelancer
+    @user = User.new(sign_up_params.merge(role: :freelancer))
+    if @user.save
+      redirect_to root_path, notice: 'Freelancer was successfully created.'
+    else
+      render :new_freelancer
     end
   end
-  
+
+  # DELETE /users/:id
+  def destroy
+    user = User.find(params[:id])
+    if current_user.admin? && !user.admin?
+      user.destroy
+      redirect_to root_path, notice: 'User was successfully deleted.'
+    else
+      redirect_to root_path, alert: 'You are not authorized to perform this action.'
+    end
+  end
+
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, :role])
+  end
+
+  private
+
+  def ensure_admin
+    unless current_user&.admin?
+      redirect_to root_path, alert: 'You are not authorized to perform this action.'
+    end
+  end
+end
