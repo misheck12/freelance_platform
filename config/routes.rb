ca0a9578417e2e43fa007c60ea598ef7ede826e8
Misheck Livingi <livingim2018@gmail.com>
2023-10-15 07:00:30 +0000

    Oct 15, 2023, 10:00 AM

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   get 'dashboard/show'
 
-  # Custom routes for freelancer registration by admin
-  get 'users/new_freelancer', to: 'users/registrations#new_freelancer', as: :new_freelancer_registration
-  post 'users/create_freelancer', to: 'users/registrations#create_freelancer', as: :create_freelancer_registration
-  delete 'users/:id', to: 'users/registrations#destroy', as: :admin_destroy_user
-
-
-  # Devise routes
+  # Standard Devise routes
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
-  # Route for destroying users
-  resources :users, only: [:destroy]
+  # Custom routes for Devise within a user scope
+  devise_scope :user do
+    get 'users/new_freelancer', to: 'users/registrations#new_freelancer', as: :new_freelancer_registration
+    post 'users/create_freelancer', to: 'users/registrations#create_freelancer', as: :create_freelancer_registration
+    delete 'users/:id', to: 'users/registrations#destroy', as: :admin_destroy_user
+  end
     
   resources :tasks
   resources :reviews, only: [:show, :new, :create, :edit, :update, :destroy]
