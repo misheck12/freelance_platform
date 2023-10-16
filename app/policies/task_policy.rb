45ae44298807f2a8f9461d0b1e91f09761207b23
Misheck Livingi <livingim2018@gmail.com>
2023-10-16 18:59:47 +0000

    Oct 16, 2023, 9:59 PM

--- app/policies/task_policy.rb
+++ app/policies/task_policy.rb
@@ -1,22 +1,22 @@
 class TaskPolicy < ApplicationPolicy
   # Define user's ability to view the task
   def show?
-    user.admin? || record.client == user || (user.freelancer? && record.freelancer == user)
+    user_is_admin_or_owner? || (user.freelancer? && record.freelancer == user)
   end
 
   # Define user's ability to create a task
   def create?
-    user.client?
+    user&.client?
   end
 
   # Define user's ability to update the task
   def update?
-    user.admin? || record.client == user
+    user_is_admin_or_owner?
   end
 
   # Define user's ability to delete the task
   def destroy?
-    user.admin? || record.client == user
+    user_is_admin_or_owner?
   end
 
   # Scope to determine which tasks a user can view in a list
@@ -29,15 +29,24 @@ class TaskPolicy < ApplicationPolicy
     end
 
     def resolve
-      if user.admin?
+      if user.nil?
+        scope.none
+      elsif user.admin?
         scope.all
       elsif user.client?
         scope.where(client: user)
       elsif user.freelancer?
-        scope.where(freelancer: user)
+        # Freelancers can see tasks assigned to them and open tasks
+        scope.where(freelancer: user).or(scope.where(status: :open))
       else
         scope.none
       end
     end
+  end
+
+  private
+
+  def user_is_admin_or_owner?
+    user&.admin? || record.client == user
   end
 end
