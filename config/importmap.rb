9f201c44d57a99a644fbe67b73a99c1cc3d8889e
Misheck Livingi <livingim2018@gmail.com>
2023-10-16 20:31:44 +0000

    Oct 16, 2023, 11:31 PM

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+pin "@rails/ujs", to: "rails-ujs.js", preload: true
+
