e4d6d04d504eb429826d9a7c81e2bc107c9ff6e9
Misheck Livingi <livingim2018@gmail.com>
2023-11-07 20:49:04 +0000

    Nov 7, 2023, 10:49 PM

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
-end
+    def badge_color_for_status(status)
+      case status
+      when 'pending'
+        'badge-secondary'
+      when 'in_progress'
+        'badge-info'
+      when 'completed'
+        'badge-success'
+      when 'cancelled'
+        'badge-danger'
+      else
+        'badge-primary'
+      end
+    end
+  end
+  
