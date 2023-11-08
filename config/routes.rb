f1c9dd6decf8dedaeb60fb4691d16744c1f29831
Misheck Livingi <livingim2018@gmail.com>
2023-11-08 10:08:08 +0000

    Nov 8, 2023, 12:08 PM

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  # Define root path
   root 'dashboard#show'
 
-  # Dashboard routes
   get 'dashboard/show'
   get 'dashboard', to: 'dashboard#show', as: 'dashboard'
 
-  # Devise routes with custom controllers for user sessions and registrations
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
-  # Custom routes within Devise for handling specific user roles like freelancer registration by admin
   devise_scope :user do
     get 'users/new_freelancer', to: 'users/registrations#new_freelancer', as: :new_freelancer_registration
     post 'users/create_freelancer', to: 'users/registrations#create_freelancer', as: :create_freelancer_registration
@@ -17,19 +13,15 @@ Rails.application.routes.draw do
     get '/users/sign_out', to: 'devise/sessions#destroy'
   end
 
-  # Task routes with custom member routes for actions like accept and complete
   resources :tasks do
     member do
       post 'accept'
       post 'complete'
     end
 
-    # Nested reviews routes under tasks for new and create actions
     resources :reviews, only: [:new, :create]
   end
 
-  # Independent reviews routes for other actions, making sure not to include :new and :create
   resources :reviews, only: [:show, :edit, :update, :destroy]
 
-  # Any other routes...
 end
