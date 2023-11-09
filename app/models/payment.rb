3a797a8a784558addd84ea373b614b493dfb1e77
Misheck Livingi <livingim2018@gmail.com>
2023-11-09 23:35:51 +0000

    Nov 10, 2023, 1:35 AM

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,4 @@
 class Payment < ApplicationRecord
-  belongs_to :user
+  belongs_to :client, class_name: 'User'
   belongs_to :task
 end
--- db/migrate/20231109233236_create_payments.rb
+++ db/migrate/20231109233236_create_payments.rb
@@ -1,7 +1,7 @@
 class CreatePayments < ActiveRecord::Migration[7.0]
   def change
     create_table :payments do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :client, foreign_key: { to_table: :users }
       t.references :task, null: false, foreign_key: true
       t.string :transaction_id
       t.integer :status
