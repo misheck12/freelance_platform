8f993b649850426879a99669023e42dd018ed85a
Misheck Livingi <livingim2018@gmail.com>
2023-11-09 09:24:44 +0000

    Nov 9, 2023, 11:24 AM

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_18_202050) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_09_092211) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_18_202050) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
+    t.text "change_requests"
     t.index ["client_id"], name: "index_tasks_on_client_id"
     t.index ["freelancer_id"], name: "index_tasks_on_freelancer_id"
   end
@@ -77,6 +78,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_18_202050) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
