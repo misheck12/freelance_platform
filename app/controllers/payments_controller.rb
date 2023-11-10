1e10400a25b751134a2c0ab4a006597e15367d85
Misheck Livingi <livingim2018@gmail.com>
2023-11-10 09:44:20 +0000

    Nov 10, 2023, 11:44 AM

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,13 +1,15 @@
-# app/controllers/payments_controller.rb
 class PaymentsController < ApplicationController
   before_action :set_task, only: [:new, :create]
 
+  # GET /tasks/:task_id/payments/new
   def new
-    @payment = @task.payments.new
+    @payment = @task.build_payment
   end
 
+  # POST /tasks/:task_id/payments
   def create
-    @payment = @task.payments.new(payment_params)
+    @payment = @task.build_payment(payment_params)
+
     if @payment.save
       redirect_to @task, notice: 'Payment was successfully created.'
     else
@@ -17,11 +19,13 @@ class PaymentsController < ApplicationController
 
   private
 
+  # Use callbacks to share common setup or constraints between actions.
   def set_task
     @task = Task.find(params[:task_id])
   end
 
+  # Only allow a list of trusted parameters through.
   def payment_params
-    params.require(:payment).permit(:transaction_id, :payment_proof)
+    params.require(:payment).permit(:transaction_id, :amount, :status)
   end
 end
