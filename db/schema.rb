fcf1720282f90d271e2f155ea55e52b5fdae313c
Misheck Livingi <livingim2018@gmail.com>
2023-11-10 11:20:20 +0000

    Nov 10, 2023, 1:20 PM

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_09_233236) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_10_111724) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,8 +49,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_09_233236) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["client_id"], name: "index_payments_on_client_id"
     t.index ["task_id"], name: "index_payments_on_task_id"
+    t.index ["user_id"], name: "index_payments_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -96,6 +98,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_09_233236) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "payments", "tasks"
+  add_foreign_key "payments", "users"
   add_foreign_key "payments", "users", column: "client_id"
   add_foreign_key "reviews", "tasks"
   add_foreign_key "reviews", "users", column: "reviewer_id"
