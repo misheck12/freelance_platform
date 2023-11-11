43610f505c2e9d740355d6a31684cfdd5165f1a4
Misheck Livingi <livingim2018@gmail.com>
2023-11-11 18:34:32 +0000

    Nov 11, 2023, 8:34 PM

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,36 +2,29 @@ class PaymentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_task
 
-  # GET /tasks/:task_id/payments/new
   def new
-    # Initialize a new payment for the task
-    @payment = @task.build_payment
+    @payment = Payment.new # Initializes a new payment
   end
 
-  # POST /tasks/:task_id/payments
   def create
-    # Build a payment for the task belonging to the current user
-    @payment = @task.build_payment(payment_params.merge(user_id: current_user.id))
+    @payment = @task.payments.build(payment_params) # Builds a payment associated with the task
+    @payment.user = current_user # Assigns the current user to the payment
 
     if @payment.save
-      # Redirect to the task with a success notice
-      redirect_to @task, notice: 'Payment was successfully created.'
+      redirect_to @task, notice: 'Payment was successfully recorded.'
     else
-      # Render the 'new' template if the payment could not be saved
       render :new
     end
   end
 
   private
 
-  # Set the task, ensuring it belongs to the current_user
   def set_task
-    @task = current_user.tasks_as_client.find(params[:task_id])
+    @task = Task.find(params[:task_id]) # Finds the task based on the passed task_id
   end
 
-  # Define permitted parameters for payments
   def payment_params
-    params.require(:payment).permit(:transaction_id, :payment_proof, :status, :user_id)
-    # Ensure :payment_proof is the correct field for the file upload
+    params.require(:payment).permit(:transaction_id, :payment_proof, :status)
+    # Ensure :payment_proof is set up to handle file uploads if it's a file field
   end
 end
