1d5103ae95bf398cfded8256668e9e570fae8fdb
Misheck Livingi <livingim2018@gmail.com>
2023-11-11 05:35:42 +0000

    Nov 11, 2023, 7:35 AM

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,5 @@
 class Payment < ApplicationRecord
-  belongs_to :client, class_name: 'User'
+  belongs_to :User
   belongs_to :task
   has_one_attached :payment_proof
   enum status: { pending: 0, approved: 1, rejected: -1 }
