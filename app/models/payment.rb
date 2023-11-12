ea81287d19df76e6ca4b8ea9c35cbef72cf7c9c9
Misheck Livingi <livingim2018@gmail.com>
2023-11-12 09:16:44 +0000

    Nov 12, 2023, 11:16 AM

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,8 @@
 class Payment < ApplicationRecord
-  belongs_to :User
+  belongs_to :user # change User to user
   belongs_to :task
   has_one_attached :payment_proof
   enum status: { pending: 0, approved: 1, rejected: -1 }
+
+  attr_accessor :user # add this line to create a user= method
 end
