f0fe448806a0c911a469b2b808bfdcdf17fc72fe
misheck12 <misheck1720@gmail.com>
2024-01-02 23:30:32 +0000

    Jan 2, 2024, 11:30 PM

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,8 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
 pin "@rails/ujs", to: "rails-ujs.js", preload: true
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/css/bootstrap.min.css"
+
 
