b974a6a57467c65e8bc615467148818af9271a22
misheck12 <misheck1720@gmail.com>
2024-01-04 11:08:04 +0000

    Jan 4, 2024, 11:08 AM

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -12,6 +12,7 @@ class PaymentsController < ApplicationController
     if @payment.nil?
       redirect_to root_path, alert: 'payment not found'
     end
+  end # <--- add this end
 
   def create
     @payment = @task.build_payment(payment_params)  # Use build_payment for has_one association
