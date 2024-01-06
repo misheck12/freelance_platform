73590f3d84cf6b793d3da21f3da2c8a8ab262f68
misheck12 <misheck1720@gmail.com>
2024-01-06 13:32:51 +0000

    Jan 6, 2024, 1:32 PM

--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -45,18 +45,18 @@ MoneyRails.configure do |config|
 
   # Register a custom currency
   #
-  # Example:
-  # config.register_currency = {
-  #   priority:            1,
-  #   iso_code:            "EU4",
-  #   name:                "Euro with subunit of 4 digits",
-  #   symbol:              "€",
-  #   symbol_first:        true,
-  #   subunit:             "Subcent",
-  #   subunit_to_unit:     10000,
-  #   thousands_separator: ".",
-  #   decimal_mark:        ","
-  # }
+  
+  config.register_currency = {
+    priority:            1,
+    iso_code:            "EU4",
+    name:                "Kwacha with subunit of 4 digits",
+    symbol:              "ZMK",
+    symbol_first:        true,
+    subunit:             "Subcent",
+    subunit_to_unit:     10000,
+    thousands_separator: ".",
+    decimal_mark:        ","
+   }
 
   # Specify a rounding mode
   # Any one of:
