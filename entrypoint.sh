e21e42203471b2d444ed1da1946d32db60dbebbc
Misheck12 <Misheck1720@gmail.com>
2024-01-19 18:19:10 +0000

    Jan 19, 2024, 8:19 PM

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-# Copy the entrypoint.sh script to /usr/bin
-COPY entrypoint.sh /usr/bin/
-
-# Make it executable
-RUN chmod +x /usr/bin/entrypoint.sh
-
-# Set it as the ENTRYPOINT
-ENTRYPOINT ["/usr/bin/entrypoint.sh"]
