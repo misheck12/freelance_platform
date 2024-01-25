9b38260202c920dde12ca84f07a735ffc031eed0
Misheck12 <Misheck1720@gmail.com>
2024-01-25 10:04:37 +0200

    push

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   # Devise routes for user authentication with custom controllers for registrations
   devise_for :users, controllers: { registrations: 'users/registrations' }
 
-  # Custom routes within Devise for handling special user registration flows
   devise_scope :user do
     get 'users/new_freelancer', to: 'users/registrations#new_freelancer', as: :new_freelancer_registration
     post 'users/create_freelancer', to: 'users/registrations#create_freelancer', as: :create_freelancer_registration
