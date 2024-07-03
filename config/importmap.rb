a915da9006c353dfbf6e56462c5ccfa2b9cb0cb8
Misheck12 <Misheck1720@gmail.com>
2024-07-03 02:09:46 +0000

    Jul 3, 2024, 4:09 AM

--- config/importmap.rb
+++ config/importmap.rb
@@ -10,5 +10,7 @@ pin "@rails/ujs", to: "rails-ujs.js", preload: true
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/css/bootstrap.min.css"
+pin "popper", to: 'popper.js', preload: true
+pin "bootstrap", to: 'bootstrap.min.js', preload: true
 
 
