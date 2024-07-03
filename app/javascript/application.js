03be5f523efb9e80794b4e866dba2975f529cbe2
Misheck12 <Misheck1720@gmail.com>
2024-07-03 02:39:08 +0000

    Jul 3, 2024, 4:39 AM

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,8 @@
 import "@hotwired/turbo-rails"
 import "controllers" // if you have Stimulus controllers you want to import
 import Rails from "@rails/ujs"
+import "popper"
+import "bootstrap"
 
 Rails.start()
 
